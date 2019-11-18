224600e50deec0cd3d7efd6ed96bac7de575078b
Robin Castellani <tecnico4.etcss@etc-eng.it>
2019-11-18 11:43:55 +0100

    New functions to make code more readable

--- Zip_files.py
+++ Zip_files.py
@@ -23,6 +23,77 @@ import argparse
 from pymongo import MongoClient
 
 
+def parse_psw_begin_end_dates():
+    """
+    CLI to parse the MongoDB password, the begin date and the end date
+    between which zip the files
+    :return:
+        the MongoDB password
+        the begin date
+        the end date
+    """
+    # get MongoDB password and date limits
+    parser = argparse.ArgumentParser(
+        description="Zip files dell'impianto; inserire password, data iniziale e finale")
+    # definizione argomenti parser
+    parser.add_argument(
+        'Password', metavar='nome', type=str,
+        help="Password di MongoDB")
+    parser.add_argument(
+        'Data_di_inizio', metavar='begin-date', type=str,
+        help='Anno, mese e giorno da cui partire per il salvataggio dei dati')
+    parser.add_argument(
+        'Data_di_fine', metavar='end-date', type=str,
+        help='Anno, mese e giorno in cui terminare il salvataggio dei dati')
+    # esecuzione del parser
+    args = parser.parse_args()
+
+    # password
+    mongodb_psw = args.Password
+    # data iniziale
+    data_iniziale = args.Data_di_inizio
+    # data finale
+    data_finale = args.Data_di_fine
+
+    # momento iniziale e finale per cui si vuole zippare i file
+    begin_date = datetime.datetime(
+        int(data_iniziale[0:4]), int(data_iniziale[4:6]),
+        int(data_iniziale[6:8]),
+        00, 00, 00)
+
+    end_date = datetime.datetime(
+        int(data_finale[0:4]), int(data_finale[4:6]),
+        int(data_finale[6:8]),
+        23, 59, 59)
+
+    return mongodb_psw, begin_date, end_date
+
+
+def connect_to_db(psw):
+    """
+    Connect to MongoDB
+    :param psw:
+        MongoDB password
+    :return:
+        document generator
+    """
+
+    # connection to MongoDB
+    connection_string = str(
+        'mongodb+srv://robin-c:{}'.format(psw) +
+        '@info-scaricamento-dati-l4hzo.gcp.mongodb.net/test?retryWrites=true&w=majority'
+    )
+    db_name = 'dati-impianti'
+    collection_name = 'dati-impianti'
+
+    client = MongoClient(connection_string)
+    collection = client[db_name][collection_name]
+
+    print('Connesso al db {}\nCollezione {}\n- - - - - - -\n'
+          .format(db_name, collection_name))
+
+    return collection.find()
+
 def convert_date(timestamp):
     """Converte la data di tipo timestamp in data da datetime
     Restituisce la data datetime
@@ -70,61 +141,17 @@ def zip_files(lista_to_zip, nome_impianto, date, dest_folder):
 
 # ---------------------------
 
-# get MongoDB password and date limits
-parser = argparse.ArgumentParser(
-    description="Zip files dell'impianto; inserire password, data iniziale e finale")
-# definizione argomenti parser
-parser.add_argument(
-    'Password', metavar='nome', type=str,
-    help="Password di MongoDB")
-parser.add_argument(
-    'Data_di_inizio', metavar='begin-date', type=str,
-    help='Anno, mese e giorno da cui partire per il salvataggio dei dati')
-parser.add_argument(
-    'Data_di_fine', metavar='end-date', type=str,
-    help='Anno, mese e giorno in cui terminare il salvataggio dei dati')
-# esecuzione del parser
-args = parser.parse_args()
-
-# password
-mongodb_psw = args.Password
-# data iniziale
-data_iniziale = args.Data_di_inizio
-# data finale
-data_finale = args.Data_di_fine
-
-# momento iniziale e finale per cui si vuole zippare i file
-FIRST_DATE = datetime.datetime(
-    int(data_iniziale[0:4]), int(data_iniziale[4:6]),
-    int(data_iniziale[6:8]),
-    00, 00, 00)
-
-LAST_DATE = datetime.datetime(
-    int(data_finale[0:4]), int(data_finale[4:6]),
-    int(data_finale[6:8]),
-    23, 59, 59)
+mongodb_psw, FIRST_DATE, LAST_DATE = parse_psw_begin_end_dates()
 
 # delta di tempo
 TIME_DELTA = relativedelta(months=1)
 
 correzione_fuso_orario = datetime.timedelta(hours=2)
 
-# connection to MongoDB
-connection_string = str(
-    'mongodb+srv://robin-c:{}'.format(mongodb_psw) +
-    '@info-scaricamento-dati-l4hzo.gcp.mongodb.net/test?retryWrites=true&w=majority'
-    )
-db_name = 'dati-impianti'
-collection_name = 'dati-impianti'
-
-client = MongoClient(connection_string)
-collection = client[db_name][collection_name]
-
-print('Connesso al db {}\nCollezione {}\n- - - - - - -\n'
-      .format(db_name, collection_name))
+plants_documents = connect_to_db(mongodb_psw)
 
 # ciclo su tutti gli impianti
-for document in collection.find():
+for document in plants_documents:
 
     # cartella contenente i file da zippare
     folder = document[u'dati_grezzi_path']
