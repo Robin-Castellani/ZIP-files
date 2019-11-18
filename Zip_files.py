ccbf40a406bf04d1a74955bf5e6f76c2bf501b18
Robin Castellani <tecnico4.etcss@etc-eng.it>
2019-11-18 11:57:58 +0100

    New functions to make code more readable

--- Zip_files.py
+++ Zip_files.py
@@ -14,132 +14,10 @@ Lo script NON elimina i file dalla cartella dei dati grezzi.
 """
 
 from __future__ import print_function
-import os
-import zipfile
 from glob import glob
 from dateutil.relativedelta import relativedelta
-import datetime
-import argparse
-from pymongo import MongoClient
+from functions import *
 
-
-def parse_psw_begin_end_dates():
-    """
-    CLI to parse the MongoDB password, the begin date and the end date
-    between which zip the files
-    :return:
-        the MongoDB password
-        the begin date
-        the end date
-    """
-    # get MongoDB password and date limits
-    parser = argparse.ArgumentParser(
-        description="Zip files dell'impianto; inserire password, data iniziale e finale")
-    # definizione argomenti parser
-    parser.add_argument(
-        'Password', metavar='nome', type=str,
-        help="Password di MongoDB")
-    parser.add_argument(
-        'Data_di_inizio', metavar='begin-date', type=str,
-        help='Anno, mese e giorno da cui partire per il salvataggio dei dati')
-    parser.add_argument(
-        'Data_di_fine', metavar='end-date', type=str,
-        help='Anno, mese e giorno in cui terminare il salvataggio dei dati')
-    # esecuzione del parser
-    args = parser.parse_args()
-
-    # password
-    mongodb_psw = args.Password
-    # data iniziale
-    data_iniziale = args.Data_di_inizio
-    # data finale
-    data_finale = args.Data_di_fine
-
-    # momento iniziale e finale per cui si vuole zippare i file
-    begin_date = datetime.datetime(
-        int(data_iniziale[0:4]), int(data_iniziale[4:6]),
-        int(data_iniziale[6:8]),
-        00, 00, 00)
-
-    end_date = datetime.datetime(
-        int(data_finale[0:4]), int(data_finale[4:6]),
-        int(data_finale[6:8]),
-        23, 59, 59)
-
-    return mongodb_psw, begin_date, end_date
-
-
-def connect_to_db(psw):
-    """
-    Connect to MongoDB
-    :param psw:
-        MongoDB password
-    :return:
-        document generator
-    """
-
-    # connection to MongoDB
-    connection_string = str(
-        'mongodb+srv://robin-c:{}'.format(psw) +
-        '@info-scaricamento-dati-l4hzo.gcp.mongodb.net/test?retryWrites=true&w=majority'
-    )
-    db_name = 'dati-impianti'
-    collection_name = 'dati-impianti'
-
-    client = MongoClient(connection_string)
-    collection = client[db_name][collection_name]
-
-    print('Connesso al db {}\nCollezione {}\n- - - - - - -\n'
-          .format(db_name, collection_name))
-
-    return collection.find()
-
-def convert_date(timestamp):
-    """Converte la data di tipo timestamp in data da datetime
-    Restituisce la data datetime
-    """
-
-    d = datetime.datetime.utcfromtimestamp(timestamp)
-    return d
-
-
-def zip_files(lista_to_zip, nome_impianto, date, dest_folder):
-    """Funzione che zippa i files di un timedelta
-    Effettua verifiche:
-    è già presente lo stesso file?
-    ci sono file da zippare?
-
-    :returns zip_name
-        name of the zip file
-    """
-    
-    # zip dei files
-    zip_name = '{}__{}.zip'.format(nome_impianto, date)
-    print('Zippando {}'.format(zip_name))
-
-    # nel caso in cui lo zip sia già presente, lo salta
-    if zip_name in os.listdir(dest_folder):
-        print('{} già presente'. format(zip_name))
-        return ''
-
-    # nel caso in cui non ci siano file da zippare
-    if not lista_to_zip:
-        print('Nessun file da zippare in {}'.format(zip_name))
-        return ''
-
-    with zipfile.ZipFile(os.path.join(dest_folder, zip_name), 'w',
-                         compression=zipfile.ZIP_DEFLATED) as new_z:
-
-        for file_to_zip in lista_to_zip:
-            new_z.write(os.path.basename(file_to_zip))
-
-        print('Zippaggio terminato {}'.format(date))
-        print('------------')
-
-    return zip_name
-
-
-# ---------------------------
 
 mongodb_psw, FIRST_DATE, LAST_DATE = parse_psw_begin_end_dates()
 
@@ -174,7 +52,7 @@ for document in plants_documents:
     # ciclo per raccogliere tutti i file contenuti in ogni TIME_DELTA
     while start_date < LAST_DATE:
 
-        for file_csv in glob('{}\*.csv'.format(folder)):
+        for file_csv in glob('{}\\*.csv'.format(folder)):
 
             data_file = (convert_date(os.path.getmtime(file_csv))
                          + correzione_fuso_orario)
@@ -208,7 +86,7 @@ for document in plants_documents:
                      'End: {}'.format(LAST_DATE)]))
 
     # zippaggio dei files
-    for date, lista_to_zip in to_zip_all.iteritems():
+    for date, lista_to_zip in to_zip_all.items():
 
         zip_name = zip_files(lista_to_zip, nome_impianto, date, dest_folder)
 
@@ -231,17 +109,3 @@ for document in plants_documents:
 
         for file_to_delete in lista_to_zip:
             os.remove(file_to_delete)
-
-    '''
-    # informazioni per ogni file contenente in ogni file .zip
-    for file_name in glob('{}\*.zip'.format(dest_folder)):
-        with zipfile.zipfile(file_name, 'r') as zip:
-            for info in zip.infolist():
-                print(info.filename)
-                print('\tmodified:\t' + str(datetime.datetime(*info.date_time)))
-                print('\tsystem:\t\t' + str(info.create_system) +
-                      '(0 = windows, 3 = unix)')
-                print('\tzip version:\t' + str(info.create_version))
-                print('\tcompressed:\t' + str(info.compress_size) + ' bytes')
-                print('\tUncompressed:\t' + str(info.file_size) + ' bytes')
-    '''
